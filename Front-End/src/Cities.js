1425be08300c17f81362a81bdb1851ea467d29f8
Gaabicarp <Gaabicarp@gmail.com>
2019-11-13 12:18:49 -0300

    Mostrar json en cities.js

--- Front-End/src/Cities.js
+++ Front-End/src/Cities.js
@@ -4,12 +4,49 @@ import Footer from "./Componentes/Footer";
 class Cities extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      loading: false,
+      cities: [],
+      url: "http://localhost:4000/city/all"
+    };
   }
 
+  componentDidMount() {
+    this.getCities();
+  }
+
+  getCities = () => {
+    this.setState({ loading: true });
+
+    fetch(this.state.url)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          cities: data.respuesta,
+          loading: false
+        });
+      })
+      .catch(e => console.log(e));
+  };
+
   render() {
+    console.log(this.state.cities);
+
+    if (this.state.loading) {
+      return (
+        <div className="principal">
+          <p>Descargando Api perro!</p>
+        </div>
+      );
+    }
+
     return (
       <div className="principal">
-        <h1>CITIES</h1>
+        <h1>asdas</h1>
+        {this.state.cities.map(city => {
+          return <li key={city._id}>{city.ciudad}</li>;
+        })}
         <Footer />
       </div>
     );
